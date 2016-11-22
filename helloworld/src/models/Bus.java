893306aa16a6282f6bb4f5010a27f882f86c89ce
Andrei Horak <linkyndy@gmail.com>
2016-11-22 21:54:15 +0100

    Add duplication to sample project

--- helloworld/src/models/Bus.java
+++ helloworld/src/models/Bus.java
@@ -8,4 +8,13 @@ public class Bus extends Vehicle{
 	public Integer getNumberOfWheels() {
 		return 4;
 	}
+	public void justADuplicateMethod() {
+		int a = 5;
+		int b = 6;
+		int c = a + b;
+		int d;
+		if (c == 10) {
+			d = 1;
+		}
+	}
 }
--- helloworld/src/models/Car.java
+++ helloworld/src/models/Car.java
@@ -9,4 +9,14 @@ public class Car extends Vehicle{
 		return 4;
 	}
 	
+	public void justADuplicateMethod() {
+		int a = 5;
+		int b = 6;
+		int c = a + b;
+		int d;
+		if (c == 10) {
+			d = 1;
+		}
+	}
+
 }
